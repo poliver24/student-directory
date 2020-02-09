e8888762014b07e69cdbf1515f6f48df2efb50d0
Patrick Oliver <poliver24@hotmail.com>
2020-02-09 21:13:14 +0000

    names with index option added

---
+++ with_index.rb
@@ -0,0 +1,37 @@
+def input_students
+  puts "Please enter the nsmes of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each.with_index do |student, index|
+    puts "#{index}.#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# call methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
